0843d81f9cef36c1ff686617a807a28e7de96cd6
Robert Williams <robert@a3marketing.com>
2020-06-08 18:26:52 -0500

    finished html routes

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -6,3 +6,13 @@ router.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "..", "/public/html/index.html"));
 });
 
+router.get("/stats", function(req, res) {
+    res.sendFile(path.join(__dirname, "..", "/public/html/stats.html"));
+});
+
+router.get("/exercise", function(req, res) {
+    res.sendFile(path.join(__dirname, "..", "/public/html/exercise.html"));
+});
+
+
+module.exports = router;
