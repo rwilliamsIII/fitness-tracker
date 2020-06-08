60acf76ce596066dc81595fcd10c472492acf189
Robert Williams <robert@a3marketing.com>
2020-06-08 17:42:24 -0500

    created update route for workout

--- routes/dbroutes.js
+++ routes/dbroutes.js
@@ -15,3 +15,24 @@ router.post("/api/workouts", ({ body }, res) => {
         res.status(400).json(err);
     });
 });
+
+router.put("/api/workouts/:id", (req, res) => {
+    console.log("Workout updated!");
+    let selected = Workout.where({ _id: req.params.id });
+    console.log(req.params.id);
+    Workout.updateOne(query,
+        {
+            $set: {
+                day: new Date().setDate(new Date().getDate()),
+            },
+            $push: {
+                exercises: { type: req.body.type, name: req.body.name, duration: req.body.duration, weight: req.body.weight, sets: req.body.sets, reps: req.body.reps, distance: req.body.distance }
+            }})
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.status(400).json(err);
+                console.log(err);
+            });
+});
